702456fc8f0753b637fbfe25e4cc7e3ea1005454
alyssahellrung <alyssahellrung@gmail.com>
2020-06-01 19:05:28 -0700

    fixing bugs

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 var express = require("express");
 
-var PORT = process.env.PORT||8080;
+var PORT = process.env.PORT || 8080;
 
 var app = express();
 
 // Serve static content for the app from the "public" directory in the application directory.
-app.use(express.static(__dirname + "/public"));
+app.use(express.static("public"));
 
 // Parse application body
 app.use(express.urlencoded({ extended: true }));
@@ -22,8 +22,6 @@ var routes = require("./controllers/burgers_controller.js");
 
 app.use(routes);
 
-// Start our server so that it can begin listening to client requests.
 app.listen(PORT, function() {
-  // Log (server-side) when our server has started
   console.log("Server listening on: http://localhost:" + PORT);
 });
